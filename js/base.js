34f3f7c68f84c9f6eb0fbb68e1be7541c94cf8fa
Ben Del Vacchio <delvach@gmail.com>
2013-08-25 13:12:10 -0600

    Finalizing navigation code

--- js/base.js
+++ js/base.js
@@ -1,20 +1,35 @@
 var nav_map = {
     home:{
         url:'/',
-        title:'SiP Boulder',
-        partial:'carousel'
+        title:'Home',
+        pageTitle:'SiP Boulder',
+        partial:'carousel',
+        init:function() {
+            $('.carousel').carousel({interval:30000})
+        }
     },
     menu:{
-        url:'/menu.html',
-        title:'SiP Boulder',
-        partial:'menu'
+        url:'/breakfast_menu.html',
+        title:'Coffee & Breakfast',
+        pageTitle:'SiP Boulder',
+        partial:'menu',
+        init:function() {}
+    },
+    drinks:{
+        url:'/drinks.html',
+        title:'Drinks & Appetizers',
+        pageTitle:'SiP Boulder - Drinks',
+        partial:'drinks',
+        init:function() {}
     }
+
 };
 
 
 function getPageData(path) {
     if(path == '/' || path == '/index.html') return nav_map.home;
     for(nav_id in nav_map) {
+        console.log(path + ', ' + nav_map[nav_id].url);
         if(path == nav_map[nav_id].url) return nav_map[nav_id];
     }
     return nav_map.home;
@@ -25,22 +40,29 @@ function getPageData(path) {
 
 
 $(function() {
-    var anchor, page = getPageData(window.location);
-    document.title = page.title;
+    var page = getPageData(window.location.pathname);
 
+    $('#nav').load('partials/nav.html', function() {
+        var nav  = $('ul#bs-nav-list');
+        document.title = page.title;
 
-    $('#nav').load('partials/nav.html', function(){
-        $('#bs-nav-list > li').each(function() {
-            anchor = $(this).find('a');
-            if(anchor.attr('href') == page.url) {
-                $(this).addClass('active');
+        $.each(nav_map, function(idx, val) {
+            if(val.url != '/') {
+                var li = $('<li/>');
+                if(page.url == val.url) li.addClass('active');
+                var a = $('<a/>').appendTo(li).text(val.title).attr('href',val.url);
+                li.appendTo(nav);
+
             }
+        })
 
-        });
+
     });
 
 
-    $('#main').load('partials/' + page.partial + '.html', function() {});
+
+    // Load page-based partial
+    $('#main').load('partials/' + page.partial + '.html', page.init ? page.init : function() {});
 
     $('#footer').load('partials/footer.html', function() {});
 });
