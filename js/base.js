abae71cdf8701dae9ce2f5b405922b5267c6e1e3
Ben Del Vacchio <delvach@gmail.com>
2013-08-25 14:14:47 -0600

    Successfully migrated navigation data to separate json file.

--- js/base.js
+++ js/base.js
@@ -1,68 +1,61 @@
-var nav_map = {
-    home:{
-        url:'/',
-        title:'Home',
-        pageTitle:'SiP Boulder',
-        partial:'carousel',
-        init:function() {
-            $('.carousel').carousel({interval:30000})
-        }
-    },
-    menu:{
-        url:'/breakfast_menu.html',
-        title:'Coffee & Breakfast',
-        pageTitle:'SiP Boulder',
-        partial:'menu',
-        init:function() {}
-    },
-    drinks:{
-        url:'/drinks.html',
-        title:'Drinks & Appetizers',
-        pageTitle:'SiP Boulder - Drinks',
-        partial:'drinks',
-        init:function() {}
+
+
+function getPageData(path, nav_data) {
+    if(path == '/' || path == '/index.html') return nav_data.home;
+    for(nav_id in nav_data) {
+        console.log(path + ', ' + nav_data[nav_id].url);
+        if(path == nav_data[nav_id].url) return nav_data[nav_id];
     }
-
-};
-
-
-function getPageData(path) {
-    if(path == '/' || path == '/index.html') return nav_map.home;
-    for(nav_id in nav_map) {
-        console.log(path + ', ' + nav_map[nav_id].url);
-        if(path == nav_map[nav_id].url) return nav_map[nav_id];
-    }
-    return nav_map.home;
+    return nav_data.home;
 }
 
 
 
-
-
 $(function() {
-    var page = getPageData(window.location.pathname);
 
     $('#nav').load('partials/nav.html', function() {
-        var nav  = $('ul#bs-nav-list');
-        document.title = page.title;
 
-        $.each(nav_map, function(idx, val) {
-            if(val.url != '/') {
-                var li = $('<li/>');
-                if(page.url == val.url) li.addClass('active');
-                var a = $('<a/>').appendTo(li).text(val.title).attr('href',val.url);
-                li.appendTo(nav);
+        $.getJSON('data/nav.json', function(nav_data) {
 
-            }
+            var curr_page = getPageData(window.location.pathname, nav_data);
+            var nav  = $('ul#bs-nav-list');
+            document.title = curr_page.title;
+
+            $.each(nav_data, function(idx, val) {
+                if(val.url != '/') {
+                    var li = $('<li/>');
+                    if(curr_page.url == val.url) li.addClass('active');
+                    var a = $('<a/>').appendTo(li).text(val.title).attr('href',val.url);
+                    li.appendTo(nav);
+
+                }
+            })
+
+            $('#main').load(
+                'partials/' + curr_page.partial + '.html',
+                curr_page.init ? eval(curr_page.init) : function() {}
+            );
+
+
         })
-
+        .fail(function( jqxhr, textStatus, error ) {
+          var err = textStatus + ', ' + error;
+          console.log( "Request Failed: " + err);
+        });
 
     });
 
 
 
     // Load page-based partial
-    $('#main').load('partials/' + page.partial + '.html', page.init ? page.init : function() {});
+    //$('#main').load('partials/' + page.partial + '.html', page.init ? page.init : function() {});
 
     $('#footer').load('partials/footer.html', function() {});
 });
+
+
+function initCarousel() {
+    setTimeout(
+        function() { $("#sip-carousel").carousel({interval:5000}) },
+        1000);
+}
