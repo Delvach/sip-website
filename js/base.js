6dbe97bc0113edc4a64d84a7439754a386c7302e
Ben Del Vacchio <delvach@gmail.com>
2013-08-25 22:04:44 -0600

    Fleshing out menu contents and formatting

--- js/base.js
+++ js/base.js
@@ -3,7 +3,7 @@
 function getPageData(path, nav_data) {
     if(path == '/' || path == '/index.html') return nav_data.home;
     for(nav_id in nav_data) {
-        console.log(path + ', ' + nav_data[nav_id].url);
+        //console.log(path + ', ' + nav_data[nav_id].url);
         if(path == nav_data[nav_id].url) return nav_data[nav_id];
     }
     return nav_data.home;
@@ -12,29 +12,32 @@ function getPageData(path, nav_data) {
 
 
 $(function() {
-
+    $.ajax({
+        cache:false
+    });
     $('#nav').load('partials/nav.html', function() {
 
         $.getJSON('data/nav.json', function(nav_data) {
 
             var curr_page = getPageData(window.location.pathname, nav_data);
             var nav  = $('ul#bs-nav-list');
-            document.title = curr_page.title;
+            document.title = curr_page.pageTitle;
 
             $.each(nav_data, function(idx, val) {
-                if(val.url != '/') {
+//                if(val.url != '/') {
                     var li = $('<li/>');
                     if(curr_page.url == val.url) li.addClass('active');
                     var a = $('<a/>').appendTo(li).text(val.title).attr('href',val.url);
                     li.appendTo(nav);
 
-                }
+//                }
             })
+            if(curr_page.init) eval(curr_page.init);
 
-            $('#main').load(
-                'partials/' + curr_page.partial + '.html',
-                curr_page.init ? eval(curr_page.init) : function() {}
-            );
+//            $('#main').load(
+//                'partials/' + curr_page.partial + '.html',
+//                curr_page.init ? eval(curr_page.init) : function() {}
+//            );
 
 
         })
@@ -50,6 +53,8 @@ $(function() {
     // Load page-based partial
     //$('#main').load('partials/' + page.partial + '.html', page.init ? page.init : function() {});
 
+
+
     $('#footer').load('partials/footer.html', function() {});
 });
 
@@ -59,3 +64,80 @@ function initCarousel() {
         function() { $("#sip-carousel").carousel({interval:5000}) },
         1000);
 }
+
+function initMorningMenu() {
+    $.getJSON('data/coffee.json', {cache:false}, function(menu_data) {
+        console.log(menu_data);
+        createMenu('#sip-menu', menu_data);
+    })
+     .fail(function( jqxhr, textStatus, error ) {
+      var err = textStatus + ', ' + error;
+      console.log( "Request Failed: " + err);
+    });
+}
+
+function createMenu(containerID, menu) {
+    var descriptionContents,
+        full_item,
+        h1,
+        h2,
+        span,
+        strong,
+        table,
+        tableBody,
+        tr,
+        td1, td2, td3, target = $(containerID);
+    for(section in menu) {
+        h1 = $('<h1>').text(menu[section].category);
+        //h2 = $('<h2>').text(menu[section].sizes);
+        //span = $('<span>', {text:menu[section].sizes,class:'pull-right'});
+        target.append(h1);
+        table = $('<table>', {class:'table menu-table'});
+        //target.append(h1.append($('<span>').text(' ('+ menu[section].sizes + ')')));
+
+        tableBody = $('<thead>');
+        td1 = $('<th>', {class:'food_col_1'});
+        td2 = $('<th>', {text:menu[section].sizes.sm,class:'food_col_2'});
+        td3 = $('<th>', {text:menu[section].sizes.lg,class:'food_col_3'});
+        table.append(tableBody.append($('<tr>').append(td1).append(td2).append(td3)));
+
+        for(i in menu[section].items) {
+            strong = $('<strong>', {text:menu[section].items[i].title});
+            descriptionContents = menu[section].items[i].description ?
+                {text:menu[section].items[i].description} :
+                {html:'&nbsp;'}
+            full_item = $('<div>')
+                .append($('<strong>', {text:menu[section].items[i].title}))
+                .append($('<div>', descriptionContents));
+            td1 = $('<td>', {class:'food_col_1'}).append(full_item);
+            td2 = $('<td>', {class:'food_col_2', text:menu[section].items[i].price.sm});
+            td3 = $('<td>', {class:'food_col_3', text:menu[section].items[i].price.lg});
+            table.append(tableBody.append($('<tr>').append(td1).append(td2).append(td3)));
+            //target.append($('<div>').text(menu[section].items[i].title));
+        }
+        target.append(table);
+        //console.log(menu[section].category);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
